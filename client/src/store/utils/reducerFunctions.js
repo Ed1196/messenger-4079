460fefe307040b565b57ac3a3c547baf962ec1b3
Ed1196 <34897031+Ed1196@users.noreply.github.com>
2021-11-05 16:51:17 -0400

    Solved bugs for the following scenarios - Showing a new unread message for a new convo on the receiver user when he has nothing typed on the Search chats bar - Showing a new unread message for a new convo on the receiver user when he has something typed on the Search chats bar The scenarios arise when we are creating new conversations, but the search feature causes them to either create a duplicate or not showing new incoming messages.

--- client/src/store/utils/reducerFunctions.js
+++ client/src/store/utils/reducerFunctions.js
@@ -2,26 +2,31 @@ export const addMessageToStore = (state, payload) => {
   const { message, sender } = payload;
   // if sender isn't null, that means the message needs to be put in a brand new convo
   if (sender !== null) {
-    return state.map((convo) => {
-      if(sender.id === convo.otherUser.id) {
-        const newConvo = {
-          id: message.conversationId,
-          otherUser: sender,
-          messages: [message],
-          unread: payload.userId ? 0 : 1,
-        };
-        newConvo.latestMessageText = message.text;
-        newConvo.latestReadByOtherId = null;
-        return newConvo;
-      } else {
-        return convo;
-      }
-    });
-    // return [newConvo, ...state];
+    const newConvo = {
+      id: message.conversationId,
+      otherUser: sender,
+      messages: [message],
+      unread: payload.userId ? 0 : 1,
+      latestMessageText: message.text,
+      latestReadByOtherId: null,
+    };
+    const findIndex = state.findIndex((c) => c.otherUser.id === sender.id);
+    if (state[findIndex]) {
+      const updatedConvos = [ ...state ];
+      updatedConvos[findIndex].id = newConvo.id;
+      updatedConvos[findIndex].latestMessageText = message.text;
+      updatedConvos[findIndex].latestReadByOtherId = null;
+      updatedConvos[findIndex].messages = [message];
+      updatedConvos[findIndex].unread = payload.userId ? 0 : 1;
+      return updatedConvos;
+    } else {
+      console.log("HERE");
+      return [newConvo, ...state];
+    }
   }
   return state.map((convo) => {
     if (convo.id === message.conversationId) {
-      const updateConvo = {...convo};
+      const updateConvo = { ...convo };
       updateConvo.messages.push(message);
       updateConvo.latestMessageText = message.text;
       updateConvo.unread = payload.userId ? 0 : updateConvo.unread + 1;
@@ -34,11 +39,11 @@ export const addMessageToStore = (state, payload) => {
 
 export const updateMessageInStore = (state, convoId) => {
   return state.map((convo) => {
-    if(convo.id === convoId) {
-      const updateConvo = {...convo};
-      const messages = [...convo.messages]
+    if (convo.id === convoId) {
+      const updateConvo = { ...convo };
+      const messages = [...convo.messages];
       const indx = updateConvo.messages.length - 1;
-      const updateMessage = {...messages[indx], read:true};
+      const updateMessage = { ...messages[indx], read: true };
       convo.messages[indx] = updateMessage;
       updateConvo.latestReadByOtherId = updateMessage.id;
       return updateConvo;
@@ -46,7 +51,7 @@ export const updateMessageInStore = (state, convoId) => {
       return convo;
     }
   });
-}
+};
 
 export const addOnlineUserToStore = (state, id) => {
   return state.map((convo) => {
